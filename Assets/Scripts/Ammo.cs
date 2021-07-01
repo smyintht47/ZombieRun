75a0049a3207c489373c958e8d98ee487e45010e
smyintht47 <46727379+smyintht47@users.noreply.github.com>
2021-07-01 00:44:12 -0400

    Add Project

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] ammoSlot[] ammoSlots;
+    [System.Serializable]
+    private class ammoSlot
+    {
+        public AmmoType ammoType;
+        public int ammoAmount;
+       
+    }
+
+    public int GetCurrentAmmo(AmmoType ammoType)
+    {
+         return GetAmmoSlot(ammoType).ammoAmount;
+    }  
+
+    public void ReduceCurrentAmmo(AmmoType ammoType)
+    {
+        GetAmmoSlot(ammoType).ammoAmount--;
+    }
+
+    public void IncreaseCurrentAmmo(AmmoType ammoType, int ammoAmount)
+    {
+        GetAmmoSlot(ammoType).ammoAmount += ammoAmount;
+    }
+
+    private ammoSlot GetAmmoSlot(AmmoType ammoType)
+    {
+        foreach (ammoSlot slot in ammoSlots)
+        {
+            if (slot.ammoType == ammoType)
+            {
+                return slot;
+            }
+        }
+        return null;
+    }
+}
